347f68e8451d7fab861425912a3b58a37e0cace1
EC2 Default User <117186071+csncad@users.noreply.github.com>
2022-12-01 07:20:19 +0000

    makingapp11Q

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,8 +8,6 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
     redirect_to list_path(list.id)
   end
 
@@ -35,6 +33,15 @@ class ListsController < ApplicationController
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
     redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   private
